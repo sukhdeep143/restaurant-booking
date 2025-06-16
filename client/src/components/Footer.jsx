6f1d10e52c3a93f030ef8eeb82cb8e6b7a5cf8dd
Sukhdeep Singh <sukhdeepsingh141414@gmail.com>
2025-06-16 22:23:17 +0530

    Components folder is added

---
+++ client/src/components/Footer.jsx
@@ -0,0 +1,11 @@
+import React from 'react';
+
+function Footer() {
+  return (
+    <footer style={{ padding: '10px 20px', backgroundColor: '#333', color: 'white', position: 'fixed', bottom: 0, width: '100%' }}>
+      <p>&copy; 2025 Restaurant Website with Booking. All rights reserved.</p>
+    </footer>
+  );
+}
+
+export default Footer;
