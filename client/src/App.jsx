0582fd652ee55853275438f7c5d0355b31307b9a
Sukhdeep Singh <sukhdeepsingh141414@gmail.com>
2025-07-16 12:51:10 +0530

    update

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -118,16 +118,18 @@
 
 import React from 'react';
 import { Routes, Route, BrowserRouter } from 'react-router-dom';
-import { AuthProvider } from './utils/AuthContext'; // Correct import
+import { AuthProvider } from './utils/AuthContext';
+
 import LoginPage from './pages/LoginPage';
 import SignUpPage from './pages/SignUpPage';
 import ForgotPassword from './pages/ForgetPassword';
 import ProtectRoute from './pages/ProtectRoute';
 import Home from './pages/Home';
 import VerifyEmailPage from './pages/VerifyEmail';
+
 import Footer from './components/Footer';
 import Header from './components/Header';
-import UserProfile from './components/UserProfile';
+import UserProfile from './pages/UserProfile';
 import UserPanel from './pages/UserPanel';
 import AdminDashboard from './pages/AdminDashboard';
 
